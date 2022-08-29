6c0dd8b265db52694930deb6838b1c59f9502db7
Artur Palha <artur.palha@gmail.com>
2022-08-29 15:12:51 +0200

    Corrects the delay between the threads in openMP

--- advanced_pybind11_features/cpp/src/interpolate.cpp
+++ advanced_pybind11_features/cpp/src/interpolate.cpp
@@ -10,10 +10,10 @@ namespace interpolate {
 
   double multilinear_adaptive_cpu_interpolator::get_point_data(double value){
     omp_set_num_threads(THREAD_NUM);
+    std::cout << "Number of available threads: " << omp_get_num_threads() << std::endl;
     #pragma omp parallel
     {
-      usleep(5000 * omp_get_num_threads()); // do this to avoid race condition while printing
-      std::cout << "Number of available threads: " << omp_get_num_threads() << std::endl;
+      usleep(200000 * omp_get_thread_num()); // do this to avoid race condition while printing
       // each thread can also get its own number
       std::cout << "Current thread number: " << omp_get_thread_num() << std::endl;
       std::cout << "Hello, World! " << this->supporting_point_evaluator->evaluate(value) + this->supporting_point_evaluator->evaluate(value) << "\n"<< std::endl;
